cd694f726e9d0def59fecdef1990ece4287da638
mfmsuhail <mfmsuhail@gmail.com>
2024-09-27 18:50:01 +0530

    DEVOPS-245: Auto scale using custom metrics

--- variables.tf
+++ variables.tf
@@ -16,12 +16,16 @@ variable "max_capacity" {
 
 variable "cpu_target_value" {
   type    = number
-  default = 60
+  default = null
 }
 
 variable "memory_target_value" {
   type    = number
-  default = 60
+  default = null
+}
+
+variable "custom_metric_target_value" {
+  type = number
 }
 
 variable "scale_in_cooldown" {
@@ -29,7 +33,27 @@ variable "scale_in_cooldown" {
   default = 300
 }
 
+variable "custom_metric_scale_in_cooldown" {
+  type = number
+}
+
 variable "scale_out_cooldown" {
   type    = number
   default = 60
+}
+
+variable "custom_metric_scale_out_cooldown" {
+  type = number
+}
+
+variable "metric_name" {
+  type = string
+}
+
+variable "namespace" {
+  type = string
+}
+
+variable "statistic" {
+  type = string
 }
